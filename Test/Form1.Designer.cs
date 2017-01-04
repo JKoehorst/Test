d2006ae5cbaafbfab1543e3d81285defcbec1103
JKoehorst <koehorst@BUSMA.local>
2017-01-04 14:54:48 +0100

    Rename button

--- Test/Form1.Designer.cs
+++ Test/Form1.Designer.cs
@@ -36,9 +36,9 @@
             // 
             this.button1.Location = new System.Drawing.Point(12, 227);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(125, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Ok";
+            this.button1.Text = "Inc. day";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
